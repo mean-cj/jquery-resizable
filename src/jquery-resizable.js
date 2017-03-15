69199b9bf0584f2c5cd700d9d54bf15fc905a842
mean-cj <register@ireallyhost.com>
2017-03-16 00:07:25 +0700

    Update jquery-resizable.js

--- src/jquery-resizable.js
+++ src/jquery-resizable.js
@@ -1,164 +1,163 @@
 /// <reference path="jquery.js" />
 /*
 jquery-resizable
-Version 0.20 - 3/10/2017
-© 2015-2017 Rick Strahl, West Wind Technologies
-www.west-wind.com
+Inspired By
+RickStrahl/jquery-resizable
+https://github.com/RickStrahl/jquery-resizable
 Licensed under MIT License
 */
-(function(factory, undefined) {
-	if (typeof define === 'function' && define.amd) {
-		// AMD
-		define(['jquery'], factory);
-	} else if (typeof module === 'object' && typeof module.exports === 'object') {
-		// CommonJS
-		module.exports = factory(require('jquery'));
-	} else {
-		// Global jQuery
-		factory(jQuery);
-	}
-}(function($, undefined) {
-	
-    function getHandle(selector, $el) {
-        if (selector && selector.trim()[0] === ">") {
-            selector = selector.trim().replace(/^>\s*/, "");
+(function($, window, document, undefined) {
 
-            return $el.find(selector);
-        }
-
-        return selector ? $(selector) : $el;
+    var defaults = {
+        // selector for handle that starts dragging
+        handleSelector: null,
+        // resize the width
+        resizeWidth: true,
+        // resize the height
+        resizeHeight: true,
+        // the side that the width resizing is relative to
+        resizeWidthFrom: 'right',
+        // the side that the height resizing is relative to
+        resizeHeightFrom: 'bottom',
+        // hook into start drag operation (event passed)
+        onDragStart: null,
+        // hook into stop drag operation (event passed)
+        onDragEnd: null,
+        // hook into each drag operation (event passed)
+        onDrag: null,
+        // disable touch-action on $handle
+        // prevents browser level actions like forward back gestures
+        touchActionNone: true
     }
 
-    if ($.fn.resizable)
-        return;
+    // The actual plugin constructor.
+    function resizable(el, options) {
 
-    $.fn.resizable = function fnResizable(options) {
-        var opt = {
-            // selector for handle that starts dragging
-            handleSelector: null,
-            // resize the width
-            resizeWidth: true,
-            // resize the height
-            resizeHeight: true,            
-            // the side that the width resizing is relative to
-            resizeWidthFrom: 'right',
-            // the side that the height resizing is relative to
-            resizeHeightFrom: 'bottom',            
-            // hook into start drag operation (event passed)
-            onDragStart: null,
-            // hook into stop drag operation (event passed)
-            onDragEnd: null,
-            // hook into each drag operation (event passed)
-            onDrag: null,
-            // disable touch-action on $handle
-            // prevents browser level actions like forward back gestures
-            touchActionNone: true
-        };
-        if (typeof options == "object") opt = $.extend(opt, options);
+        this.options = $.extend(true, {}, defaults, options);
+        this.$el = $(el);
 
-        return this.each(function () {
-            var startPos, startTransition;
+        this._defaults = defaults;
+        this._name = 'resizable';
 
-            var $el = $(this);
+        this.init();
+    }
 
-            var $handle = getHandle(opt.handleSelector, $el);
+    // Initialize all parts of the plugin.
+    $.extend(resizable.prototype, {
+        init: function() {
+            this.$handle = this.options.handleSelector ? this.$el.find(this.options.handleSelector) : this.$el;
+            if (this.options.touchActionNone) this.$handle.css("touch-action", "none");
+            this.$el.addClass("resizable");
+            this.$handle.on('mousedown.rsz touchstart.rsz',  $.proxy(this.startDragging,this));
+        },
+        startDragging: function(e,v) {
+            // Prevent dragging a ghost image in HTML5 / Firefox and maybe others
+            if (e.preventDefault) {  e.preventDefault();  }
 
-            if (opt.touchActionNone)
-                $handle.css("touch-action", "none");
+            startPos = this.getMousePos(e);
+            startPos.width = parseInt(this.$el.width(), 10);
+            startPos.height = parseInt(this.$el.height(), 10);
 
-            $el.addClass("resizable");
-            $handle.bind('mousedown.rsz touchstart.rsz', startDragging);
+            this.startTransition = this.$el.css("transition");
+            this.$el.css("transition", "none");
 
-            function noop(e) {
-                e.stopPropagation();
-                e.preventDefault();
+            if (this.options.onDragStart) {
+                if (this.options.onDragStart(e) === false)
+                    return;
+            }
+
+
+            $(document).on('mousemove.rsz', $.proxy(this.doDrag,this));
+            $(document).on('mouseup.rsz', $.proxy(this.stopDragging,this));
+            if (window.Touch || navigator.maxTouchPoints) {
+                $(document).on('touchmove.rsz', $.proxy(this.doDrag,this));
+                $(document).on('touchend.rsz', $.proxy(this.stopDragging,this));
+            }
+            $(document).on('selectstart.rsz', this.noop); // disable selection
+        },
+        noop: function(e) {
+            e.stopPropagation();
+            e.preventDefault();
+        },
+        doDrag: function(e) {
+
+            var pos = this.getMousePos(e),
+                newWidth, newHeight;
+
+            if (this.options.resizeWidthFrom === 'left')
+                newWidth = startPos.width - pos.x + startPos.x;
+            else
+                newWidth = startPos.width + pos.x - startPos.x;
+
+            if (this.options.resizeHeightFrom === 'top')
+                newHeight = startPos.height - pos.y + startPos.y;
+            else
+                newHeight = startPos.height + pos.y - startPos.y;
+
+            if (!this.options.onDrag || this.options.onDrag(e, this.$el, newWidth, newHeight, this.options) !== false) {
+                if (this.options.resizeHeight)
+                    this.$el.height(newHeight);
+
+                if (this.options.resizeWidth)
+                    this.$el.width(newWidth);
+            }
+        },
+        stopDragging: function(e) {
+            e.stopPropagation();
+            e.preventDefault();
+
+            $(document).off('mousemove.rsz');
+            $(document).off('mouseup.rsz');
+
+            if (window.Touch || navigator.maxTouchPoints) {
+                $(document).off('touchmove.rsz');
+                $(document).off('touchend.rsz');
+            }
+            $(document).off('selectstart.rsz');
+
+            // reset changed values
+            this.$el.css("transition", this.startTransition);
+
+            if (this.options.onDragEnd)
+                this.options.onDragEnd(e, this.$el, this.options);
+
+            return false;
+        },
+        getMousePos: function(e) {
+            var pos = {
+                x: 0,
+                y: 0,
+                width: 0,
+                height: 0
             };
+            if (typeof e.clientX === "number") {
+                pos.x = e.clientX;
+                pos.y = e.clientY;
+            } else if (e.originalEvent.touches) {
+                pos.x = e.originalEvent.touches[0].clientX;
+                pos.y = e.originalEvent.touches[0].clientY;
+            } else
+                return null;
 
-            function startDragging(e) {
-                // Prevent dragging a ghost image in HTML5 / Firefox and maybe others    
-                if ( e.preventDefault ) {
-                  e.preventDefault();
-                }
-                
-                startPos = getMousePos(e);
-                startPos.width = parseInt($el.width(), 10);
-                startPos.height = parseInt($el.height(), 10);
+            return pos;
+        }
+    });
 
-                startTransition = $el.css("transition");
-                $el.css("transition", "none");
 
-                if (opt.onDragStart) {
-                    if (opt.onDragStart(e, $el, opt) === false)
-                        return;
-                }
-                opt.dragFunc = doDrag;
-
-                $(document).bind('mousemove.rsz', opt.dragFunc);
-                $(document).bind('mouseup.rsz', stopDragging);
-                if (window.Touch || navigator.maxTouchPoints) {
-                    $(document).bind('touchmove.rsz', opt.dragFunc);
-                    $(document).bind('touchend.rsz', stopDragging);
-                }
-                $(document).bind('selectstart.rsz', noop); // disable selection
-            }
-
-            function doDrag(e) {
-                var pos = getMousePos(e), newWidth, newHeight;
-
-                if (opt.resizeWidthFrom === 'left')
-                    newWidth = startPos.width - pos.x + startPos.x;
-                else
-                    newWidth = startPos.width + pos.x - startPos.x;
-
-                if (opt.resizeHeightFrom === 'top')
-                    newHeight = startPos.height - pos.y + startPos.y;
-                else
-                    newHeight = startPos.height + pos.y - startPos.y;
-
-                if (!opt.onDrag || opt.onDrag(e, $el, newWidth, newHeight, opt) !== false) {
-                    if (opt.resizeHeight)
-                        $el.height(newHeight);                    
-
-                    if (opt.resizeWidth)
-                        $el.width(newWidth);                    
-                }
-            }
-
-            function stopDragging(e) {
-                e.stopPropagation();
-                e.preventDefault();
-
-                $(document).unbind('mousemove.rsz', opt.dragFunc);
-                $(document).unbind('mouseup.rsz', stopDragging);
-
-                if (window.Touch || navigator.maxTouchPoints) {
-                    $(document).unbind('touchmove.rsz', opt.dragFunc);
-                    $(document).unbind('touchend.rsz', stopDragging);
-                }
-                $(document).unbind('selectstart.rsz', noop);
-
-                // reset changed values
-                $el.css("transition", startTransition);
-
-                if (opt.onDragEnd)
-                    opt.onDragEnd(e, $el, opt);
-
-                return false;
-            }
-
-            function getMousePos(e) {
-                var pos = { x: 0, y: 0, width: 0, height: 0 };
-                if (typeof e.clientX === "number") {
-                    pos.x = e.clientX;
-                    pos.y = e.clientY;
-                } else if (e.originalEvent.touches) {
-                    pos.x = e.originalEvent.touches[0].clientX;
-                    pos.y = e.originalEvent.touches[0].clientY;
-                } else
-                    return null;
-
-                return pos;
+    // A plugin wrapper around the constructor.
+    // Pass `options` with all settings that are different from the default.
+    // The attribute is used to prevent multiple instantiations of the plugin.
+    $.fn.resizable = function(options) {
+        var attribute = 'resizable';
+        return this.each(function() {
+            // Prevent against multiple instantiations.
+            var instance = $.data(this, attribute);
+            if (!instance) {
+                instance = new resizable(this, options);
+                $.data(this, attribute, instance);
             }
         });
     };
-}));
+
+
+})(jQuery, window, document);
